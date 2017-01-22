6528775fd8a3b4ec59b807a4cd9b2dfac36c79e3
Naoyuki Miyazaki <miyazaki@buddying.jp>
2017-01-22 23:05:33 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+let WebSocketServer = require('ws').Server;
+let port = 3001;
+let wsServer = new WebSocketServer({ port: port })
+console.log(`websocket server start. port=${port}`)
+
+wsServer.on('connection', ws => {
+  console.log('-- websocket connected --')
+  ws.on('message', message => {
+    wsServer.clients.forEach(client => {
+      if (isSame(ws, client)) {
+        console.log('- skip sender -')
+      } else {
+        client.send(message);
+      }
+    })
+  })
+})
+
+function isSame(ws1, ws2) {
+  // -- compare object --
+  return (ws1 === ws2);
+}
